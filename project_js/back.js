f93657235c98f31af13b3806f6d49bad6574247a
xomolya <dp131192pka@gmail.com>
2019-11-23 18:29:35 +0200

    slider

---
+++ project_js/back.js
@@ -0,0 +1,63 @@
+// "use strict"
+// // 1)Number
+
+// var num1 = 5;
+// var num2 = 10;
+
+// var result = num1 + num2;
+
+// console.log(result);
+
+// console.log(typeof result);
+
+// // 2) String
+
+// var a = "строка 1";
+// var b = " строка 2";
+
+// console.log(typeof (a + b))
+
+// // 3)boolean
+
+// var res = 5 > 6;
+// console.log(res);
+
+// // 4)undefined
+// var undef;
+// console.log(undef)
+
+// // 5) Null
+
+
+// var undef2 = null;
+
+// console.log(typeof undef2);
+
+// // 6) Object array
+
+// var names = ['vasa', "andrew", "ann"];
+
+// names.push('olya', 'katya');
+
+// names[1] = 'ilya'
+
+// console.log(names[1]);
+
+// // 6) object object
+
+// var user = {
+//     name: 'Andrew',
+//     age: 25,
+//     email: "sada@gmail.com"
+// };
+
+// console.log(user.name);
+
+
+// var a = 5 - "ddd";
+
+// console.log(a)
+
+
+
+
